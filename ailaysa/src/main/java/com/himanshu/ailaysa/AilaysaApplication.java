e88c6da361cca9c840d69f75f2720cb2cb8af0cd
himanshuptl007 <himanshuptl786@gmail.com>
2023-07-27 19:17:00 +0530

    Task Completed

---
+++ ailaysa/src/main/java/com/himanshu/ailaysa/AilaysaApplication.java
@@ -0,0 +1,12 @@
+package com.himanshu.ailaysa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilaysaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AilaysaApplication.class, args);
+	}
+}
---
+++ ailaysa/src/main/java/com/himanshu/ailaysa/controller/MarkdownContentController.java
@@ -0,0 +1,28 @@
+package com.himanshu.ailaysa.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.text.TextContentRenderer;
+
+@RestController
+public class MarkdownContentController {
+
+    @PostMapping("/markdowncontent")
+    public ResponseEntity<String> getMarkdownContent(@RequestBody String markdownContent) {
+        String plainTextContent = convertMarkdownToPlainText(markdownContent);
+        return ResponseEntity.ok(plainTextContent);
+    }
+
+    private String convertMarkdownToPlainText(String markdownContent) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdownContent);
+
+        TextContentRenderer textContentRenderer = TextContentRenderer.builder().build();
+        return textContentRenderer.render(document);
+    }
+}
+
